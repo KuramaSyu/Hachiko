7458bc2d45cf6633dafc644d0c318fa55ec358b0
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-08-19 17:08:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Hosting;
+using NetCord;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Rest;
+
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddDiscordGateway(options =>
+{
+    options.Token = builder.Configuration["Discord:Token"]!;
+}).AddApplicationCommands();
+
+var host = builder.Build();
+
+host.AddUserCommand("Username", (User user) => user.Username)
+    .AddMessageCommand("Length", (RestMessage m) => m.Content.Length.ToString());
+
+// Add commands from modules
+host.AddModules(typeof(Program).Assembly);
+
+await host.RunAsync();
+
